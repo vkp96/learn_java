023d8bbdd422be506d5156688788ea54fd320993
Vivek <vivekpareek096@gmail.com>
2021-01-14 16:00:59 +0530

    waiting for completable future to complete

--- CompleteStageAPI/src/FirstCompletableFutureClass.java
+++ CompleteStageAPI/src/FirstCompletableFutureClass.java
@@ -3,9 +3,11 @@ import java.util.concurrent.CompletableFuture;
 class FirstCompletableFutureClass {
     //The following code does not print anything since the main thread dies before the completable future
     //gets a chance to execute
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         CompletableFuture.runAsync(() -> {
            System.out.println("I am running asynchronously!");
         });
+        //the below statement allows some time for the runnable task to execute
+        Thread.sleep(100);
     }
 }
